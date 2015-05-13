735f324dd71ca547d40d4cdd1f8c5d4cf32c5e8d
Tahantos <jokkend@gmail.comgit config --global user.email jokkend@gmail.com>
2015-05-13 13:08:46 +0300

    opengl init completed

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=doublebeat
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/doublebeat
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/doublebeat
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=doublebeat.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/doublebeat.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/doublebeat.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,84 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
 #include <SDL2/SDL_opengl.h>
+#include <GL/glu.h>
 #include <Box2D/Box2D.h>
 #include "utils/PropertyUtil.h"
 
-const int SCREEN_WIDTH = 1366;
-const int SCREEN_HEIGHT = 768;
+int SCREEN_WIDTH = 1366;
+int SCREEN_HEIGHT = 768;
 const char* title = "Double Beat";
 
 void logSDLError(std::ostream &os, const std::string &msg) {
     os << msg << "error:" << SDL_GetError() << std::endl;
 }
+
+void setupOpenGL(int width, int height) {
+    float ratio = (float) width / (float) height;
+    
+    glShadeModel(GL_SMOOTH);
+    
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+    glEnable(GL_CULL_FACE);
+    
+    glClearColor(0,0,0,0);
+    
+    glViewport(0, 0, width, height);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    gluPerspective(60.0, ratio, 1.0, 1024.0);
+}
+
+int setWorkspaceResolution() {
+    RECT rect;
+    
+    if(SystemParametersInfo(SPI_GETWORKAREA, 0, &rect, 0) == 0) {
+        logSDLError(std::cout, "GetWorkArea");
+        return 1;
+    }
+    
+    SCREEN_WIDTH = rect.right;
+    SCREEN_HEIGHT = rect.bottom;
+    
+    return 0;
+}
+
 int main(int argc, char** argv) {
+    
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    
     if(SDL_Init(SDL_INIT_TIMER|SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_JOYSTICK|SDL_INIT_GAMECONTROLLER|SDL_INIT_EVENTS) == -1) {
         logSDLError(std::cout, "SDL_init");
         return 1;
     } 
+    
+    setWorkspaceResolution();
+    
     SDL_Window *win = 0;
+    SDL_GLContext con;
+    
     win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if(win==0) {
         logSDLError(std::cout, "CreateWindow");
         return 1;
     }
+    
+    con = SDL_GL_CreateContext(win);
+    
+    SDL_GL_SetSwapInterval(1);
+    
+    setupOpenGL(SCREEN_WIDTH, SCREEN_HEIGHT);
+    
     SDL_Event event;
     bool done = false;
     
