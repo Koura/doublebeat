42d3f300690dfc0cea959b0c5967137e97499ff5
Tahantos <jokkend@gmail.comgit config --global user.email jokkend@gmail.com>
2015-05-16 14:50:19 +0300

    simple trinagle draw

---
+++ src/draw/glDraw.cpp
@@ -0,0 +1,22 @@
+
+#include <GL/gl.h>
+
+#include "glDraw.h"
+
+glDraw::drawSimpleTriangle(float x, float y, float red, float green, float blue, float size) {
+    glPushMatrix();
+    
+    glTranslatef(x, y, 0.0f);
+    
+    glBegin(GL_TRIANGLES);
+    
+    glColor4f(red, green, blue, 1);
+    glVertex2f(x, size+y);
+    glVertex2f(-size+x, -size+y);
+    glVertex2f(size+x, -size+y);
+    
+    glEnd();
+    
+    glPopMatrix();
+}
+
---
+++ src/draw/glDraw.h
@@ -0,0 +1,14 @@
+#ifndef GLDRAW_H
+#define	GLDRAW_H
+#include <string>
+#include <stdlib.h>
+
+class glDraw {
+public:
+    static drawSimpleTriangle(float x, float y, float red, float green, float blue, float size);
+private:
+
+};
+
+#endif	
+
